07f0008cc53b5183ac0eb96bf3ac6d0cbdae9162
Shunji Zhan <shunhahaha@live.com>
2021-07-12 12:05:16 +0800

    post page routing and content
    -move post.vue to /template folder
    - in config, route /post/:id to post.vue compoent
    - in post.vue, use page-query to get the post data, using the $id param. Then render these data

--- gridsome.config.js
+++ gridsome.config.js
@@ -20,6 +20,7 @@ module.exports = {
         apiURL: 'http://localhost:1337',
         queryLimit: 1000, // Defaults to 100
         contentTypes: ['post'],
+        typename: 'Strapi',
         // singleTypes: ['impressum'],
         // Possibility to login with a Strapi user,
         // when content types are not publicly available (optional).
@@ -29,5 +30,13 @@ module.exports = {
         // }
       }
     }
-  ]
+  ],
+  templates: {
+    StrapiPost: [   // typename + contentType
+      {
+        path: '/post/:id',
+        component: './src/templates/Post.vue',
+      }
+    ],
+  }
 }
---
+++ src/templates/Post.vue
@@ -0,0 +1,59 @@
+<template>
+  <layout>
+    <!-- Page Header -->
+    <header class="masthead" :style="{ backgroundImage: `url(http://localhost:1337${$page.curPost.cover.url})  ` }">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-heading">
+              <h1>{{ $page.curPost.title }}</h1>
+              <span class="meta"
+                >Posted by
+                <a href="#">xxx</a>
+                {{ $page.curPost.created_at }}</span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Post Content -->
+    <article>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto" v-html="$page.curPost.content">
+          </div>
+        </div>
+      </div>
+    </article>
+  </layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+	curPost: strapiPost (id: $id) {
+    id
+    title
+    created_at
+    content
+    cover {
+      url
+    }
+    tags {
+      id
+      title
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: 'PostPage',
+}
+</script>
+
+<style>
+</style>
