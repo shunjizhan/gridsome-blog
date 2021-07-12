1bfd61f6b1b664156294602ae3aedeb315c6e815
Shunji Zhan <shunhahaha@live.com>
2021-07-12 11:17:46 +0800

    display prefetched data in index page
    - added <page-qeury> in index page, which will fetch data from graphql.
    - iterate these data and display them in the page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,63 +19,25 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="post-preview">
+          <div class="post-preview" v-for="edge in $page.posts.edges" :key="edge.node.id">
             <a href="post.html">
               <h2 class="post-title">
-                Man must explore, and this is exploration at its greatest
+                {{ edge.node.title}}
               </h2>
-              <h3 class="post-subtitle">
+              <!-- <h3 class="post-subtitle">
                 Problems look mighty small from 150 miles up
-              </h3>
+              </h3> -->
             </a>
             <p class="post-meta">
               Posted by
-              <a href="#">Start Bootstrap</a>
-              on September 24, 2019
+              <a href="#">author name</a>
+               {{ edge.node.created_at }}
             </p>
-          </div>
-          <hr />
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">
-                I believe every human has a finite number of heartbeats. I don't
-                intend to waste any of mine.
-              </h2>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#">Start Bootstrap</a>
-              on September 18, 2019
-            </p>
-          </div>
-          <hr />
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">Science has not yet mastered prophecy</h2>
-              <h3 class="post-subtitle">
-                We predict too much for the next year and yet far too little for
-                the next ten.
-              </h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#">Start Bootstrap</a>
-              on August 24, 2019
-            </p>
-          </div>
-          <hr />
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">Failure is not an option</h2>
-              <h3 class="post-subtitle">
-                Many say exploration is part of our destiny, but it’s actually
-                our duty to future generations.
-              </h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#">Start Bootstrap</a>
-              on July 8, 2019
+            <p>
+              <span v-for="tag in edge.node.tags" :key="tag.id">
+                <a href="" >{{ tag.title }}</a>
+                &nbsp;
+              </span>
             </p>
           </div>
           <hr />
@@ -90,6 +52,24 @@
     </div>
   </Layout>
 </template>
+
+<page-query>
+query {
+	posts: allStrapiPost {
+    edges {
+      node {
+        id
+        title
+				created_at
+        tags {
+          id
+          title
+        }
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
